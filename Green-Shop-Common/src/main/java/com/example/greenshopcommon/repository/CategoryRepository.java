895d65a31717a962c07d31746d1c34036f1a59e4
Comp <ovojan84@gmail.com>
2023-06-30 23:12:25 +0400

    category endPoint

--- Green-Shop-Common/src/main/java/com/example/greenshopcommon/repository/CategoryRepository.java
+++ Green-Shop-Common/src/main/java/com/example/greenshopcommon/repository/CategoryRepository.java
@@ -4,6 +4,9 @@ package com.example.greenshopcommon.repository;
 import com.example.greenshopcommon.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
+    Optional<Category> findByName(String name);
 }
